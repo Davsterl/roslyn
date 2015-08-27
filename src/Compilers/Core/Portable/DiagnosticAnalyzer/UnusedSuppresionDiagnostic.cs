835ee20d831b7b20ad11615bf515526df1ef5672
David Sterling <dsterling@live.com>
2015-08-27 10:15:36 -0700

    Adding in tests for the Unsuppressed Global Attributes. Formalized the Diagnostic that's returned

---
+++ src/Compilers/Core/Portable/DiagnosticAnalyzer/UnusedSuppresionDiagnostic.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.CodeAnalysis
+{
+   internal class UnusedSuppresionDiagnostic
+    {
+        private static readonly LocalizableString s_localizableMessage = new LocalizableResourceString(nameof(CodeAnalysisResources.UnnecessarySuppressionMessage), CodeAnalysisResources.ResourceManager, typeof(CodeAnalysisResources));
+
+        private static readonly LocalizableString s_localizableTitleUnnecessarySuppression = new LocalizableResourceString(nameof(CodeAnalysisResources.UnnecessarySuppressionTitle), CodeAnalysisResources.ResourceManager, typeof(CodeAnalysisResources));
+        private static readonly LocalizableString s_localizableCategory = new LocalizableString(nameof(CodeAnalysisResources.UnnecessarySuppressionCategory), CodeAnalysisResources.ResourceManager, typeof(CodeAnalysisResources));
+        private static readonly DiagnosticDescriptor s_descriptorUnnecessarySuppression = new DiagnosticDescriptor("RS9000",
+                                                                    s_localizableTitleUnnecessarySuppression.ToString(),
+                                                                    s_localizableMessage.ToString(),
+                                                                    s_localizableCategory.ToString(),
+                                                                    DiagnosticSeverity.Hidden,
+                                                                    isEnabledByDefault: true,
+                                                                    customTags: WellKnownDiagnosticTags.Unnecessary);
+
+        static DiagnosticDescriptor descriptor = new DiagnosticDescriptor("RS2001", "Suppression Message unnecessary", "SuppressMessage Attribute is unnesscessary", "Suppression", DiagnosticSeverity.Warning, true);
+
+        private UnusedSuppresionDiagnostic(){}
+        internal static Diagnostic Create (Location location)
+        {
+           return Diagnostic.Create(s_descriptorUnnecessarySuppression, location);
+        }
+    }
+}
